bd7be3e54299b051ab02afd2d9acf97ae137bf47
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2023-05-27 18:33:56 +0200

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+import unittest
+import subprocess
+
+class TestCalculator(unittest.TestCase):
+    def test_multiplicacion(self):
+        res = subprocess.check_output(['python3', 'calculadora.py', '5', '3'])
+        res = res.decode().strip()
+        self.assertEqual(res, "Resultado: 15")
+
+if __name__ == '__main__':
+    unittest.main()
