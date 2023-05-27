4f7403e99fdbf6dba60d1d1e9e516e7942a30b93
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2023-05-27 18:00:15 +0200

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+import sys
+
+def multiplicar(multiplicando, multiplicador):
+
+    return multiplicando * multiplicador
+
+if __name__ == '__main__':
+       multi1 = float(sys.argv[1])
+       multi2 = float(sys.argv[2])
+       res = multiplicar(multi1, multi2)
+       print("Resultado: ",res)
