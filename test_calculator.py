104edd6c0a938b3b90b0033672f6849be3560549
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2023-05-27 18:37:03 +0200

    Add files via upload

--- test_calculator.py
+++ test_calculator.py
@@ -5,7 +5,7 @@ class TestCalculator(unittest.TestCase):
     def test_multiplicacion(self):
         res = subprocess.check_output(['python3', 'calculadora.py', '5', '3'])
         res = res.decode().strip()
-        self.assertEqual(res, "Resultado: 15")
+        self.assertEqual(res, "Resultado: 15.0")
 
 if __name__ == '__main__':
     unittest.main()
